bdafa4f899516a9372955cc7b65a922be554219e
Lucky <dtymokhin@livesoft.pro>
2022-11-20 16:52:05 +0200

    initial-commit

---
+++ src/subscribers/subscriber.entity.ts
@@ -0,0 +1,17 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn
+} from "typeorm";
+
+@Entity()
+export class Subscriber {
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column({ unique: true })
+  public email: string;
+
+  @Column()
+  public name: string;
+}
---
+++ src/subscribers/subscribers.service.ts
@@ -0,0 +1,29 @@
+import { Controller } from "@nestjs/common";
+import { GrpcMethod } from "@nestjs/microservices";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { CreateSubscriberDto } from "./create-subscriber.dto";
+import { Subscriber } from "./subscriber.entity";
+
+@Controller()
+export class SubscribersService {
+  constructor(
+    @InjectRepository(Subscriber)
+    private subscribersRepository: Repository<Subscriber>,
+  ) {}
+
+  @GrpcMethod()
+  async addSubscriber(subscriber: CreateSubscriberDto) {
+    const newSubscriber = await this.subscribersRepository.create(subscriber);
+    await this.subscribersRepository.save(newSubscriber);
+    return newSubscriber;
+  }
+
+  @GrpcMethod()
+  async getAllSubscribers() {
+    const data = await this.subscribersRepository.find();
+    return {
+      data
+    }
+  }
+}
